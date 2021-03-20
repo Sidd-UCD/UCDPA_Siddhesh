f021416f40e97729bfffee7ba15c8d860136b0c7
siddheshkotian <Siddy.kotian@gmail.com>
2021-03-20 23:20:04 +0000

    "Exploring Dataframe"

--- Data Set 1.py
+++ Data Set 1.py
@@ -3,3 +3,21 @@ import pandas as pd
 df_1 = pd.read_csv('/Users/siddheshkotian/Documents/Certification Data Analytics/Project Rubric/Data_Set A.csv')
 
 print(df_1)
+
+#Exploring Dataframe(df_1)
+
+print(df_1.head())
+
+print(df_1.info())
+
+print(df_1.shape)
+
+print(df_1.values)
+
+print(df_1.columns)
+
+print(df_1.index)
+
+
+
+
