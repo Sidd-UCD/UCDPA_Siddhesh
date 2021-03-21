7506cadbb5aae5d9b31409d50e48a313d0423439
siddheshkotian <Siddy.kotian@gmail.com>
2021-03-21 01:57:07 +0000

    "Sorting Columns"

--- Data Set 1.py
+++ Data Set 1.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+
+
 df_1 = pd.read_csv('/Users/siddheshkotian/Documents/Certification Data Analytics/Project Rubric/Data_Set A.csv')
 
 print(df_1)
@@ -18,6 +20,29 @@ print(df_1.columns)
 
 print(df_1.index)
 
+#Sorting Dataset(df_1)
+
+df_1_sales = df_1.sort_values("Sales")
+
+print(df_1_sales)
+
+print(df_1_sales.head())
+
+df_1_sales_des = df_1.sort_values(["Sales"], ascending=[False])
+
+print(df_1_sales_des)
+
+print(df_1_sales_des.head())
+
+df_1_sales_units_des = df_1.sort_values(["Sales", "Units"], ascending=[False, False])
+
+print(df_1_sales_units_des)
+
+print(df_1_sales_units_des.head())
+
+df_1_location = df_1.sort_values("Location")
+
+print(df_1_location)
 
 
 
