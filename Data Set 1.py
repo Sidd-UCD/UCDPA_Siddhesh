0d06cc7456664ca3b56a4ac0e9e23afaad4b0970
siddheshkotian <Siddy.kotian@gmail.com>
2021-03-21 17:38:43 +0000

    "Dropping Duplicates"

--- Data Set 1.py
+++ Data Set 1.py
@@ -1,12 +1,10 @@
 import pandas as pd
 
-
-
 df_1 = pd.read_csv('/Users/siddheshkotian/Documents/Certification Data Analytics/Project Rubric/Data_Set A.csv')
 
 print(df_1)
 
-#Exploring Dataframe(df_1)
+# Exploring Dataframe(df_1)
 
 print(df_1.head())
 
@@ -20,7 +18,7 @@ print(df_1.columns)
 
 print(df_1.index)
 
-#Sorting Dataset(df_1)
+# Sorting Dataset(df_1)
 
 df_1_sales = df_1.sort_values("Sales")
 
@@ -43,6 +41,22 @@ print(df_1_sales_units_des.head())
 df_1_location = df_1.sort_values("Location")
 
 print(df_1_location)
+
+# Dropping duplicates
+
+df_1_unique_location = df_1.drop_duplicates(subset="Location")
+
+print(df_1_unique_location)
+
+print(df_1_unique_location.value_counts())
+
+df_1_unique_location_sales = df_1.drop_duplicates(subset=["Location", "Sales"])
+
+print(df_1_unique_location_sales)
+
+print(df_1_unique_location_sales.sort_values("Sales", ascending=False))
+
+print(df_1_unique_location_sales.sort_values("Sales", ascending=False).head())
 
 
 
